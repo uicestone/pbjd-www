e8423ea68fe1fef84159a1110e7e801f1cc80c6d
Uice Lu <uicestone@gmail.com>
2018-06-25 13:41:30 +0800

    Quntuan home detail

--- src/renderer/views/quntuan.vue
+++ src/renderer/views/quntuan.vue
@@ -1,15 +1,19 @@
 <script>
+import { getPosts } from '../utils/request';
+
 export default {
   data() {
     return {
-      scaleItems: {}
+      scaleItems: {},
+      posts: [],
+      showingPost: null
     };
   },
   computed: {
 
   },
-  mounted() {
-
+  async mounted() {
+    this.posts = await getPosts({ query: { category: '群团服务', limit: -1 } });
   },
   methods: {
     scale(item) {
@@ -17,6 +21,15 @@ export default {
       setTimeout(() => {
         this.$set(this.scaleItems, item, false);
       }, 1000)
+    },
+    back() {
+      this.$router.go(-1);
+    },
+    showPost(slug) {
+      this.showingPost = this.posts.filter(post => post.slug === slug)[0];
+    },
+    closePost() {
+      this.showingPost = null;
     }
   }
 };
@@ -24,12 +37,23 @@ export default {
 
 <template lang="pug">
   .main.page-quntuan
-    .entries
-      a.entry(v-for="index in 8" :class="{['qt'+(index)]:true,scale:scaleItems['entry-'+index]}" @click="scale('entry-'+index)")
+    a.back(v-if="!showingPost" @click="back")
+      i.fa.fa-chevron-left
+      span 返回
+    .entries(:class="{blur:showingPost}")
+      a.entry(v-for="index in 8" :class="{['qt'+(index)]:true,scale:scaleItems['entry-'+index]}" @click="scale('entry-'+index);showPost('qt-'+index)")
         img(:src="'static/images/qt-item-'+index+'.svg'")
+    .detail(v-if="showingPost" :class="{'fade-in': showingPost}")
+      a.back(v-if="showingPost" @click="closePost")
+        i.fa.fa-chevron-left
+        span 返回
+      h1 {{ showingPost.title }}
+      .content(v-html="showingPost.content")
+
+
 </template>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
 .main.page-quntuan
   opacity 1
   height 100vh
@@ -59,6 +83,41 @@ export default {
         padding 0 1% 4% 0
       &.qt2 img
         height 47%
+    &.blur
+      filter blur(.5vw)
+  .detail
+    background rgba(111, 77, 46, 0.9)
+    position absolute
+    top 0
+    left 0
+    width 100vw
+    height 100vh
+    padding 10vw
+    overflow-y auto
+    color white
+    font-size 4vw
+    text-align justify
+    h1
+      font-size 2em
+      text-align center
+      margin-bottom 0.5em
+    .content
+      p
+        text-indent 2em
+        margin-bottom 0.5em
+      img
+        max-width 100%
+        margin 0.5em auto
+    .back
+      color white
+      text-shadow 0 0 2vw rgb(111, 77, 46)
+  .back
+    position fixed
+    font-size 4vw
+    font-weight bold
+    left 3vw
+    top 2vw
+    color #4d4d4e
 @keyframes scale
   0%
     transform scale(1)
@@ -66,6 +125,13 @@ export default {
     transform scale(1.3)
   100%
     transform scale(1)
+@keyframes fade-in
+  0%
+    opacity 0
+  100%
+    opacity 1
 .scale
   animation scale 0.3s ease-in-out
+.fade-in
+  animation fade-in 0.5s ease-in-out
 </style>
