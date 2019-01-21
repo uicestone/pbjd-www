ee9bfcc075c4c13d1f62904de525941cd1e612c0
Uice Lu <uicestone@gmail.com>
2019-01-21 22:24:11 +0800

    fix: 新用户个人中心不显示党员签到

--- src/renderer/views/geren.vue
+++ src/renderer/views/geren.vue
@@ -123,7 +123,7 @@
         <div class="item" @click="show('my-yuyue')"><i class="fa fa-map-marker"></i> 我的活动预定</div>
         <div class="item" @click="show('my-speech')"><i class="fa fa-volume-up"></i> 我的党建声音</div>
         <!-- <div class="item" @click="show('my-motto')"><i class="fa fa-volume-up"></i> 我的座右铭</div> -->
-        <div class="item" @click="$router.push('baodao')" v-if="!mySignIn"><i class="fa fa-check-circle-o"></i> 党员签到</div>
+        <div class="item" @click="$router.push('baodao')" v-if="!mySignIn.name"><i class="fa fa-check-circle-o"></i> 党员签到</div>
         <button class="btn-block" @click="logout()">退出登录</button>
       </div>
       <div class="content geren-menu my-yuyue-list" v-if="showing=='my-yuyue'">
